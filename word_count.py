91bb48f6a83dbab4be569be483c756c28a2eba64
ysgiraldo <ygiraldoca@unal.edu.co>
2024-02-23 20:40:34 -0500

    Intento 2

--- word_count.py
+++ word_count.py
@@ -125,7 +125,7 @@ def save_output(output_directory, sequence):
 # entregado como parámetro.
 #
 def create_marker(output_directory):
-    with open(output_directory + "/_SUCCES", "w") as file:
+    with open(output_directory + "/_SUCCESS", "w") as file:
         file.write("")
 
 
